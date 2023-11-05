d8b7c00900c49c72794deb0e49498c0909e145fc
unknown <dannysaravia100@gmail.com>
2023-11-05 03:31:12 -0500

    added javascript code that starts addding local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,28 +2,45 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function() {
-  $(".saveBtn").on(this).siblings("description").val();
+  // Define the current hour using Day.js
+  var currentHour = dayjs().hour();
 
-  var timeBlockId = $(this).closest(".time-block").attr("id")
-})
- var currentHour = dayjs().hour();
+  // Add a click event listener to all elements with the class "saveBtn"
+  $(".saveBtn").on("click", function() {
+    // Get the user input from the textarea inside the same time-block
+    var userInput = $(this).siblings(".description").val();
 
-$('.time-block').each(function () {
-  var timeBlockId = $(this).attr('id')
-  var blockHour = parseInt(timeBlockId)
-  
-  if (blockHour < currentHour) {
-    $(this).addClass("past")
-  }else if(blockHour === currentHour) {
-    $(this).addClass("present")
-  } else {
-    $(this).addClass("future")
-  }
+    // Get the ID of the parent time-block element
+    var timeBlockId = $(this).closest(".time-block").attr("id");
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+    // Use the timeBlockId as the key to save the user input in local storage
+    localStorage.setItem(timeBlockId, userInput);
+  });
+
+  // Apply past, present, or future classes to time-blocks based on the current hour
+  $('.time-block').each(function () {
+    var timeBlockId = $(this).attr('id');
+    var blockHour = parseInt(timeBlockId.split('-')[1]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+
+  // Retrieve and display user input from local storage
+  $(".time-block").each(function() {
+    var timeBlockId = $(this).attr("id");
+    var storedInput = localStorage.getItem(timeBlockId);
+
+    if (storedInput !== null) {
+      $(this).find(".description").val(storedInput);
+    }
+  });
+
+  // Display the current date in the header
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Module-5-challenge
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Description
+
+This project was made with some starter code to help devolopers learn how to add items to the local storage and how to grab items from the local storage and display them on the page. Developers also learned how to add real-time dates to the webpage. 
+
+## Installation
+
+N/A
+
+## Usage
+
+
+
+## Credits
+
+N/A 
+
+## License
+
+Refer to the repo for licenses. 
+
+
+## screenshots of the webpage
+![Screenshot 1](<./develop/Screenshot1.png>)
+![Screenshot 2](<./develop/Screenshot2.png>)
+
+## Webpage link 
+
+
